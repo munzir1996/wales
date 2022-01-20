e9bd5f98e5e8df27ca78e9649bb8c258bde9b9d2
M.Mukhtar96 <munzirmukhtar996@gmail.com>
2022-01-20 14:29:43 +0200

    :sparkle: upload files in wells

--- resources/views/basic-informations/show.blade.php
+++ resources/views/basic-informations/show.blade.php
@@ -200,7 +200,7 @@
                         <thead>
                             <tr>
                                 <th scope="col" class="px-6 py-3 text-right">
-                                    أسم الملف
+                                    ملفات اختبار تحليل المياه
                                 </th>
                                 <th scope="col"
                                     class="px-6 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
@@ -215,13 +215,12 @@
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <div class="flex items-center space-x-2">
-                                        <a href="{{route('basic-informations.downloadfile', $media->id)}}" class="text-gray-500 focus:outline-none hover:text-indigo-500">
+                                        <a href="{{route('basic_informations.downloadfile', $media->id)}}" class="text-gray-500 focus:outline-none hover:text-indigo-500">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                            </svg>
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
+                                              </svg>
                                         </a>
-                                        <a href="{{route('basic-informations.deletefile', [$media->id, $donorentity->id])}}" class="text-gray-500 focus:outline-none hover:text-indigo-500">
+                                        <a href="{{route('basic_informations.deletefile', [$media->id, $basicInformation->id])}}" class="text-gray-500 focus:outline-none hover:text-indigo-500">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
                                                 viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -236,10 +235,102 @@
                             @endforeach
                         </tbody>
                     </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                    <div class="w-full mt-8 bg-white dark:bg-gray-800">
+    <div class="flex flex-col mt-8">
+        <div class="-my-2 overflow-x-auto xl:-mx-8">
+            <div class="inline-block min-w-full py-2 align-right sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-right">
+                                    ملفات الدراسة الجوفيزيائية
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach ($basicInformation->getMedia('geophysical_study_file') as $media)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    {{$media->name}}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center space-x-2">
+                                        <a href="{{route('basic_informations.downloadfile', $media->id)}}" class="text-gray-500 focus:outline-none hover:text-indigo-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
+                                              </svg>
+                                        </a>
+                                        <a href="{{route('basic_informations.deletefile', [$media->id, $basicInformation->id])}}" class="text-gray-500 focus:outline-none hover:text-indigo-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                                                viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                                                </path>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                    </div>
+    <div class="flex flex-col mt-8">
+        <div class="-my-2 overflow-x-auto xl:-mx-8">
+            <div class="inline-block min-w-full py-2 align-right sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-right">
+                                    ملفات تفاصيل البئر كاملة
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach ($basicInformation->getMedia('full_well_details_file') as $media)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    {{$media->name}}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center space-x-2">
+                                        <a href="{{route('basic_informations.downloadfile', $media->id)}}" class="text-gray-500 focus:outline-none hover:text-indigo-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
+                                              </svg>
+                                        </a>
+                                        <a href="{{route('basic_informations.deletefile', [$media->id, $basicInformation->id])}}" class="text-gray-500 focus:outline-none hover:text-indigo-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                                                viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                                                </path>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
