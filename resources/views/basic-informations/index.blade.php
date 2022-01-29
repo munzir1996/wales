6f510ff4481d618f6a9e0e9b5142292c19ce2714
creaspo <creaspo23@gmail.com>
2022-01-30 00:08:42 +0200

    :construction: wip

--- resources/views/basic-informations/index.blade.php
+++ resources/views/basic-informations/index.blade.php
@@ -33,17 +33,17 @@
 
                 <!-- component -->
                 <!-- This is an example component -->
-  
+
             </div>
-            <form action="{{route('search')}}" method="GET">
+            <form action="{{ route('search') }}" method="GET">
                 @csrf
                 <div class="pt-2 relative mx-auto text-gray-600">
                     <input class="border-2 border-gray-300 bg-white h-10 px-8 pr-10 rounded-lg text-sm focus:outline-none"
-                        type="search" name="search" placeholder="بحث" value="{{request()->input('search')}}">
+                        type="search" name="search" placeholder="بحث" value="{{ request()->input('search') }}">
                     <button class="bg-indigo-500 text-white font-semie-bold py-2 px-4 rounded">بحث</button>
                 </div>
             </form>
-       
+
 
             <div class="flex flex-col mt-8">
                 <div class="-my-2 overflow-x-auto xl:-mx-8">
@@ -98,6 +98,24 @@
                                         <th scope="col"
                                             class="px-6 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
                                         </th> --}}
+
+
+                                        <th>
+                                            <a href="{{ route('report') }}">
+                                                <button class="text-gray-500 focus:outline-none hover:text-indigo-500">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                                                        </path>
+                                                    </svg>
+                                                </button>
+                                            </a>
+                                        </th>
+
+
+
                                     </tr>
                                 </thead>
 
--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -92,12 +92,9 @@
                                             class="px-6 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
                                             P.M
                                         </th>
+                                   
                                         <th scope="col"
-                                            class="px-6 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
-                                            Date
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
+                                            class="px-5 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
                                             Days
                                         </th>
                                         <th scope="col"
@@ -106,6 +103,7 @@
                                         </th>
                                         <th scope="col"
                                             class="px-6 py-3 text-sm font-medium tracking-wider text-right text-gray-700 uppercase whitespace-nowrap">
+                                            Date
                                         </th>
                                     </tr>
                                 </thead>
@@ -138,55 +136,16 @@
                                             <td class="flex-1 px-6 py-4 text-gray-500 whitespace-nowrap">
                                                 {{ $basicInformation->project_manager }}
                                             </td>
-                                            <td class="flex-1 px-6 py-4 text-gray-500 whitespace-nowrap">
-                                                {{ $basicInformation->start_date }}
-                                            </td>
+                                        
                                             <td class="flex-1 px-6 py-4 text-gray-500 whitespace-nowrap">
                                                 {{ $basicInformation->execution_time }}
                                             </td>
                                             <td class="flex-1 px-6 py-4 text-gray-500 whitespace-nowrap">
                                                 {{ $basicInformation->total_cost }}
                                             </td>
-                                             {{-- <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="flex items-center space-x-2">
-                                                    <a href="{{ route('basic-informations.show', $basicInformation->id) }}"
-                                                        class="text-gray-500 focus:outline-none hover:text-indigo-500">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
-                                                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2"
-                                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                                        </svg>
-                                                    </a>
-                                                    <a href="{{ route('basic-informations.edit', $basicInformation->id) }}"
-                                                        class="text-gray-500 focus:outline-none hover:text-indigo-500">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"
-                                                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2"
-                                                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
-                                                            </path>
-                                                        </svg>
-                                                    </a>
-                                                    <form
-                                                        action="{{ route('basic-informations.destroy', $basicInformation->id) }}"
-                                                        method="post">
-                                                        @csrf {{ method_field('DELETE') }}
-                                                        <button
-                                                            class="text-gray-500 focus:outline-none hover:text-indigo-500">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"
-                                                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                                    stroke-width="2"
-                                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
-                                                                </path>
-                                                            </svg>
-                                                        </button>
-                                                    </form>
-                                                </div>
-                                            </td> --}}
+                                            <td class="flex-1 px-6 py-4 text-gray-500 whitespace-nowrap">
+                                                {{ $basicInformation->start_date }}
+                                            </td>
                                         </tr>
                                     @endforeach
 
@@ -204,7 +163,7 @@
 
     </div>
     <br>
-<div name="page-footer" style="margin-top: 360px">
+<div name="page-footer" style="margin-top: 160px">
     <img src="{{ asset('assets/images/footer.jpeg') }}" alt="images" style="width:900px;height:100px;">
 </div>
 </body>
